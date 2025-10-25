18bc70399a54405d3a9f75aa6999f86331507434
Abhilash-Kothuru <abhi2abhi000@gmail.com>
2025-10-25 13:53:01 +0530

    Create CalculatorPage.tsx

---
+++ src/pages/CalculatorPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SolarCalculator from '../components/SolarCalculator';
+
+const CalculatorPage: React.FC = () => {
+    return (
+        <div>
+            <SolarCalculator />
+        </div>
+    );
+};
+
+export default CalculatorPage;
